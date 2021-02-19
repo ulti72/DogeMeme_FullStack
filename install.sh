0f3012590c1aa668441b4308cbd7acd6305110f8
Abhishek kumar <goole4money@gmail.com>
2021-02-19 17:25:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #MongoDB Installation
- 
 #Import the public key used by the package management system
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
- 
+
 #Create a list file for MongoDB.
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
  
@@ -18,8 +17,7 @@ sudo systemctl start mongod
  
 #Ensure that MongoDB will start following a system reboot
 sudo systemctl enable mongod
- 
- 
+
 #Node Setup
  
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -32,7 +30,6 @@ export NVM_DIR="$HOME/.nvm"
  
 #install node
 nvm install node
- 
+
 #check the version
 node --version
- 
