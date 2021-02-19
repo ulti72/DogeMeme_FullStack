2e7d160546ec6c371b44ec3024ec144e18e8c23c
Abhishek kumar <goole4money@gmail.com>
2021-02-19 19:37:16 +0530

    update

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo systemctl enable mongod
 #Node Setup
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-.~/.nvm/nvm.sh
+#.~/.nvm/nvm.sh
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
