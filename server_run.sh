519ae4f8e003515fc5ad46e625696730caef424d
Abhishek kumar <goole4money@gmail.com>
2021-02-19 17:33:19 +0530

    update server run

--- server_run.sh
+++ server_run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
 .~/.nvm/nvm.sh
-
 export NVM_DIR="$HOME/.nvm"
-
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 cd backend
-
-
 npm install
-
 node index.js
