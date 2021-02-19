579d5ddee100c2cab199ab8cbf6001f3f53fe08f
Abhishek kumar <goole4money@gmail.com>
2021-02-19 17:44:28 +0530

    delete

--- server_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-cd backend
-npm install
-node index.js
