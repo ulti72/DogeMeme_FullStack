84eb010dde125a4999ec79c2417b2c9691044bcd
Abhishek kumar <goole4money@gmail.com>
2021-02-19 11:52:08 +0530

    Update server_run

--- server_run.sh
+++ server_run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
 cd backend
 
 
-# Setup DB or any other environment variables you want to setup.
+# Install dependencies and run the app
 
 
 npm install
-
-npm run
 
 node index.js
